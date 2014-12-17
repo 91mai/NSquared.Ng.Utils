ec3ca3be504f8402daf3319ee796aaa9f036e428
hengchih <chrischang@91mai.com>
2014-12-17 16:58:18 +0800

    add rangeFilter unit test file

--- test/filters/rangeFilter.spec.js
+++ test/filters/rangeFilter.spec.js
@@ -20,13 +20,17 @@ describe('Range Filter', function () {
         };
 
         var act = {
-            createRule1 : $filter('nsRange')('a', 1, 'NSquared.Ng.Utils'),
-            useStringParameter2 : $filter('nsNumberFitLength')(1, 'b', 'NSquared.Ng.Utils')
+            createRule1 : $filter('nsRange')(emptyAry(), 1, 5, 'NSquared.Ng.Utils'),
+            createRule2 : $filter('nsRange')(emptyAry(), 4, 5, 'NSquared.Ng.Utils'),
+            createRule3 : $filter('nsRange')(emptyAry(), 1, 1, 'NSquared.Ng.Utils'),
+            createRule4 : $filter('nsRange')(emptyAry(), 5, 3, 'NSquared.Ng.Utils')
         };
 
         // Assert.
-        expect(act.useStringParameter1).toEqual('a');
-
+        expect(act.createRule1).toEqual([ 1, 2, 3, 4, 5 ]);
+        expect(act.createRule2).toEqual([ 4, 5 ]);
+        expect(act.createRule3).toEqual([ 1 ]);
+        expect(act.createRule4).toEqual([ ]);
     });
 
 });
