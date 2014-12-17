19a3f6f0cb2aebb95fd3cfc28c7db52b6a86705c
hengchih <chrischang@91mai.com>
2014-12-17 16:14:47 +0800

    delete coverage dir and ignore coverage

---
+++ test/filters/rangeFilter.spec.js
@@ -0,0 +1,32 @@
+describe('Range Filter', function () {
+    'use strict';
+
+    var $filter;
+
+    //// Mock $filter
+    beforeEach(function () {
+        module('NSquared.Ng.Utils');
+
+        inject(function (_$filter_) {
+            $filter = _$filter_;
+        });
+
+    });
+
+    it('should create a range', function () {
+
+        /*var emptyAry = function(){
+            return [];
+        };
+
+        var act = {
+            createRule1 : $filter('nsRange')('a', 1, 'NSquared.Ng.Utils'),
+            useStringParameter2 : $filter('nsNumberFitLength')(1, 'b', 'NSquared.Ng.Utils')
+        };
+
+        // Assert.
+        expect(act.useStringParameter1).toEqual('a');*/
+
+    });
+
+});
