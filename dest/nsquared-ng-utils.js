f744ccc774141d2ba571323ed1033d655a8c2687
Kirk <rwk0119@yahoo.com.tw>
2014-12-23 17:23:51 +0800

    Add Url Utility

--- dest/nsquared-ng-utils.js
+++ dest/nsquared-ng-utils.js
@@ -1,21 +1,23 @@
+/// <reference path="../typings/angularjs/angular.d.ts"/>
+/// <reference path="../typings/jquery/jquery.d.ts"/>
+
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             angular.module('NSquared.Ng.Utils', []);
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
 
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             var CELLPHONE_ONLY_REGEXP = /^09\d{8}$/;
+
             angular.module('NSquared.Ng.Utils').directive('nsOnlyCellphone', function () {
                 return {
                     require: 'ngModel',
@@ -25,8 +27,7 @@ var NSquared;
                                 // it is valid
                                 ctrl.$setValidity('onlyCellphone', true);
                                 return viewValue;
-                            }
-                            else {
+                            } else {
                                 // it is invalid, return undefined (no model update)
                                 ctrl.$setValidity('onlyCellphone', false);
                                 return undefined;
@@ -35,17 +36,18 @@ var NSquared;
                     }
                 };
             });
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
 
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             var CHINESE_ONLY_REGEXP = /^[\u4E00-\u9FA5]{2,}$/;
+
             angular.module('NSquared.Ng.Utils').directive('nsOnlyChinese', function () {
                 return {
                     require: 'ngModel',
@@ -55,8 +57,7 @@ var NSquared;
                                 // it is valid
                                 ctrl.$setValidity('onlyChinese', true);
                                 return viewValue;
-                            }
-                            else {
+                            } else {
                                 // it is invalid, return undefined (no model update)
                                 ctrl.$setValidity('onlyChinese', false);
                                 return undefined;
@@ -65,45 +66,43 @@ var NSquared;
                     }
                 };
             });
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
 
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             angular.module('NSquared.Ng.Utils').directive('nsOnlyIdentityNo', function () {
                 return {
                     require: 'ngModel',
                     link: function (scope, elm, attrs, ctrl) {
                         ctrl.$parsers.unshift(function (viewValue) {
-                            var pass = 0; /*為了通過後面的防呆所以...*/
-                            var num = new Array(11); /*用來存轉換碼加九個數字 */
-                            var sum = 0; /*計算經過公式後的加總*/
-                            var table = new Array(10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32, 30, 31, 33); /*轉換的對照表*/
+                            var pass = 0;
+                            var num = new Array(11);
+                            var sum = 0;
+
+                            var table = new Array(10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32, 30, 31, 33);
+
                             /*以下是防呆*/
                             if (viewValue.length != 10) {
                                 ctrl.$setValidity('onlyIdentityNo', false);
                                 return undefined;
-                            }
-                            else if (viewValue.charCodeAt(0) < "A".charCodeAt(0) || viewValue.charCodeAt(0) > "Z".charCodeAt(0)) {
+                            } else if (viewValue.charCodeAt(0) < "A".charCodeAt(0) || viewValue.charCodeAt(0) > "Z".charCodeAt(0)) {
                                 ctrl.$setValidity('onlyIdentityNo', false);
                                 return undefined;
-                            }
-                            else if (viewValue.charCodeAt(1) != "1".charCodeAt(0) && viewValue.charCodeAt(1) != "2".charCodeAt(0)) {
+                            } else if (viewValue.charCodeAt(1) != "1".charCodeAt(0) && viewValue.charCodeAt(1) != "2".charCodeAt(0)) {
                                 ctrl.$setValidity('onlyIdentityNo', false);
                                 return undefined;
-                            }
-                            else {
+                            } else {
                                 for (var i = 2; i < 10; i++) {
                                     if (viewValue.charCodeAt(i) < "0".charCodeAt(0) || viewValue.charCodeAt(i) > "9".charCodeAt(0)) {
                                         ctrl.$setValidity('onlyIdentityNo', false);
                                         return undefined;
-                                    }
-                                    else {
+                                    } else {
                                         pass++;
                                     }
                                 }
@@ -112,20 +111,20 @@ var NSquared;
                                 ctrl.$setValidity('onlyIdentityNo', false);
                                 return undefined;
                             }
+
                             /*以上是防呆*/
                             num[1] = table[viewValue.charCodeAt(0) - 65] % 10;
                             num[0] = (table[viewValue.charCodeAt(0) - 65] - num[1]) / 10;
                             for (i = 1; i < 10; i++)
                                 num[i + 1] = viewValue.charCodeAt(i) - 48;
                             for (i = 1; i < 9; i++)
-                                num[i] = num[i] * (10 - i);
+                                num[i] = num[i] * (10 - i); /*套用公式*/
                             for (i = 0; i < 11; i++)
                                 sum += num[i];
                             if (sum % 10 == 0) {
                                 ctrl.$setValidity('onlyIdentityNo', true);
                                 return viewValue;
-                            }
-                            else {
+                            } else {
                                 ctrl.$setValidity('onlyIdentityNo', false);
                                 return undefined;
                             }
@@ -133,15 +132,15 @@ var NSquared;
                     }
                 };
             });
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
 
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             angular.module('NSquared.Ng.Utils').directive('nsOnlyInvoiceno', function () {
                 return {
@@ -153,15 +152,18 @@ var NSquared;
                                 ctrl.$setValidity('onlyInvoiceno', false);
                                 return undefined;
                             }
+
                             //// 檢查是否為數字
                             var numberPattern = /^\d{8}$/;
                             if (!numberPattern.test(viewValue)) {
                                 ctrl.$setValidity('onlyInvoiceno', false);
                                 return undefined;
                             }
+
                             function _valid(n) {
                                 return (n % 10 == 0) ? true : false;
                             }
+
                             function _cal(n) {
                                 var sum = 0;
                                 while (n != 0) {
@@ -170,13 +172,16 @@ var NSquared;
                                 }
                                 return sum;
                             }
+
                             var tmp = new String("12121241");
                             var sum = 0;
+
                             for (var i = 0; i < 8; i++) {
                                 var s1 = parseInt(viewValue.substr(i, 1));
                                 var s2 = parseInt(tmp.substr(i, 1));
                                 sum += _cal(s1 * s2);
                             }
+
                             var isValid = false;
                             if (!_valid(sum)) {
                                 if (viewValue.substr(6, 1) == "7") {
@@ -186,6 +191,7 @@ var NSquared;
                                 }
                                 ;
                             }
+
                             isValid = _valid(sum);
                             ctrl.$setValidity('onlyInvoiceno', isValid);
                             return isValid ? viewValue : undefined;
@@ -193,15 +199,15 @@ var NSquared;
                     }
                 };
             });
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
 
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             var WHITESPACE_REGEXP = /[\s　]/ig;
             angular.module('NSquared.Ng.Utils').directive('nsRemoveWhitespace', function () {
@@ -212,6 +218,7 @@ var NSquared;
                             if (!i) {
                                 return null;
                             }
+
                             return i.replace(WHITESPACE_REGEXP, '');
                         }
                         ctrl.$parsers.push(removeWhitespace);
@@ -219,15 +226,15 @@ var NSquared;
                     }
                 };
             });
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
 
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             var NumberFitLengthFilter = (function () {
                 function NumberFitLengthFilter() {
@@ -236,9 +243,11 @@ var NSquared;
                     return function (input, length) {
                         var num = parseInt(input, 10);
                         length = parseInt(length, 10);
+
                         if (isNaN(num) || isNaN(length)) {
                             return input;
                         }
+
                         var numString = '' + num;
                         while (numString.length < length) {
                             numString = '0' + numString;
@@ -249,16 +258,17 @@ var NSquared;
                 return NumberFitLengthFilter;
             })();
             Utils.NumberFitLengthFilter = NumberFitLengthFilter;
+
             angular.module('NSquared.Ng.Utils').filter('nsNumberFitLength', ['$log', NumberFitLengthFilter.Factory]);
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
 
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             var PriceFilter = (function () {
                 function PriceFilter() {
@@ -268,16 +278,20 @@ var NSquared;
                         if (!input) {
                             return '$0';
                         }
+
                         input = parseFloat(input);
+
                         if (input % 1 === 0) {
                             input = input.toFixed(0);
                         }
+
                         return '$' + input;
                     };
                 };
                 return PriceFilter;
             })();
             Utils.PriceFilter = PriceFilter;
+
             var ZeroToNullFilter = (function () {
                 function ZeroToNullFilter() {
                 }
@@ -286,22 +300,24 @@ var NSquared;
                         if (input === 0) {
                             return null;
                         }
+
                         return input;
                     };
                 };
                 return ZeroToNullFilter;
             })();
             Utils.ZeroToNullFilter = ZeroToNullFilter;
+
             angular.module('NSquared.Ng.Utils').filter('nsPrice', ['$log', PriceFilter.Factory]).filter('nsZeroToNull', ['$log', ZeroToNullFilter.Factory]);
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
 
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             var RangeFilter = (function () {
                 function RangeFilter() {
@@ -310,25 +326,28 @@ var NSquared;
                     return function (input, min, max) {
                         min = parseInt(min); //Make string input int
                         max = parseInt(max);
+
                         for (var i = min; i <= max; i++) {
                             input.push(i);
                         }
+
                         return input;
                     };
                 };
                 return RangeFilter;
             })();
             Utils.RangeFilter = RangeFilter;
+
             angular.module('NSquared.Ng.Utils').filter('nsRange', ['$log', RangeFilter.Factory]);
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
 
 var NSquared;
 (function (NSquared) {
-    var Ng;
     (function (Ng) {
-        var Utils;
         (function (Utils) {
             var ReverseFilter = (function () {
                 function ReverseFilter() {
@@ -341,7 +360,87 @@ var NSquared;
                 return ReverseFilter;
             })();
             Utils.ReverseFilter = ReverseFilter;
+
             angular.module('NSquared.Ng.Utils').filter('nsReverse', ['$log', ReverseFilter.Factory]);
-        })(Utils = Ng.Utils || (Ng.Utils = {}));
-    })(Ng = NSquared.Ng || (NSquared.Ng = {}));
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
+})(NSquared || (NSquared = {}));
+
+var NSquared;
+(function (NSquared) {
+    (function (Ng) {
+        (function (Utils) {
+            var UrlUtility = (function () {
+                function UrlUtility() {
+                }
+                UrlUtility.prototype.GetQuerystring = function (name) {
+                    try  {
+                        var hash = document.location.hash;
+                        var match = RegExp('[?&]' + name + '=([^&]*)').exec(document.location.search);
+                        return match && decodeURIComponent(decodeURIComponent(match[1].replace(/\+/g, ' ')));
+                    } catch (e) {
+                        return null;
+                    }
+                };
+
+                //// Source code from
+                //// http://stackoverflow.com/questions/6953944/how-to-add-parameters-to-a-url-that-already-contains-other-parameters-and-maybe
+                UrlUtility.prototype.AppendParameter = function (url, key, value, atStart) {
+                    if (typeof atStart === "undefined") { atStart = false; }
+                    var replaceDuplicates = true;
+
+                    //// Get url without hash
+                    var urlhash = '';
+                    var urlLength = url.length;
+                    if (url.indexOf('#') > 0) {
+                        urlLength = url.indexOf('#');
+                        urlhash = url.substring(url.indexOf('#'), url.length);
+                    }
+
+                    var sourceUrl = url.substring(0, urlLength);
+
+                    //// Get url search part
+                    var urlParts = sourceUrl.split("?");
+                    var newQueryString = "";
+
+                    //// Get new parameter query string
+                    if (urlParts.length > 1) {
+                        var parameters = urlParts[1].split("&");
+                        for (var i = 0; (i < parameters.length); i++) {
+                            var parameterParts = parameters[i].split("=");
+                            if (!(replaceDuplicates && parameterParts[0] == key)) {
+                                if (newQueryString == "")
+                                    newQueryString = "?";
+                                else
+                                    newQueryString += "&";
+                                newQueryString += parameterParts[0] + "=" + (parameterParts[1] ? parameterParts[1] : '');
+                            }
+                        }
+                    }
+                    if (newQueryString == "")
+                        newQueryString = "?";
+
+                    //// Append to url
+                    if (atStart) {
+                        newQueryString = '?' + key + "=" + value + (newQueryString.length > 1 ? '&' + newQueryString.substring(1) : '');
+                    } else {
+                        if (newQueryString !== "" && newQueryString != '?')
+                            newQueryString += "&";
+                        newQueryString += key + "=" + (value ? value : '');
+                    }
+                    return urlParts[0] + newQueryString + urlhash;
+                };
+                return UrlUtility;
+            })();
+            Utils.UrlUtility = UrlUtility;
+
+            angular.module('NSquared.Ng.Utils').factory('UrlUtility', function () {
+                return new UrlUtility();
+            });
+        })(Ng.Utils || (Ng.Utils = {}));
+        var Utils = Ng.Utils;
+    })(NSquared.Ng || (NSquared.Ng = {}));
+    var Ng = NSquared.Ng;
 })(NSquared || (NSquared = {}));
---
+++ src/utilities/urlUtility.ts
@@ -0,0 +1,65 @@
+﻿module NSquared.Ng.Utils {
+    export class UrlUtility {
+        GetQuerystring(name: string) {
+            try {
+                var hash = document.location.hash;
+                var match = RegExp('[?&]' + name + '=([^&]*)')
+                    .exec(document.location.search);
+                return match && decodeURIComponent(decodeURIComponent(match[1].replace(/\+/g, ' ')));
+            }
+            catch (e) {
+                return null;
+            }
+        }
+
+        //// Source code from 
+        //// http://stackoverflow.com/questions/6953944/how-to-add-parameters-to-a-url-that-already-contains-other-parameters-and-maybe
+        AppendParameter(url: string, key: string, value: string, atStart= false): string {
+            var replaceDuplicates = true;
+
+            //// Get url without hash
+            var urlhash = '';
+            var urlLength = url.length;
+            if (url.indexOf('#') > 0) {
+                urlLength = url.indexOf('#');
+                urlhash = url.substring(url.indexOf('#'), url.length);
+            }
+
+            var sourceUrl = url.substring(0, urlLength);
+
+            //// Get url search part
+            var urlParts = sourceUrl.split("?");
+            var newQueryString = "";
+
+            //// Get new parameter query string
+            if (urlParts.length > 1) {
+                var parameters = urlParts[1].split("&");
+                for (var i = 0; (i < parameters.length); i++) {
+                    var parameterParts = parameters[i].split("=");
+                    if (!(replaceDuplicates && parameterParts[0] == key)) {
+                        if (newQueryString == "")
+                            newQueryString = "?";
+                        else
+                            newQueryString += "&";
+                        newQueryString += parameterParts[0] + "=" + (parameterParts[1] ? parameterParts[1] : '');
+                    }
+                }
+            }
+            if (newQueryString == "")
+                newQueryString = "?";
+
+            //// Append to url
+            if (atStart) {
+                newQueryString = '?' + key + "=" + value + (newQueryString.length > 1 ? '&' + newQueryString.substring(1) : '');
+            } else {
+                if (newQueryString !== "" && newQueryString != '?')
+                    newQueryString += "&";
+                newQueryString += key + "=" + (value ? value : '');
+            }
+            return urlParts[0] + newQueryString + urlhash;
+        }
+    }
+
+    angular.module('NSquared.Ng.Utils')
+           .factory('UrlUtility', () => new UrlUtility());
+} 
